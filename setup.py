3ad511d42764b1aa27a39c5c29c0830dac9e82f9
adamlincoln <adamjlincoln@gmail.com>
2015-06-08 23:21:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='python-bayeux-client',
       url='http://github.com/dkmadigan/python-bayeux-client',
       license="LICENSE.txt",
       long_description=open('README.md').read(),
-      install_requires=['zope.interface','twisted.internet'],
+      install_requires=['twisted'],
       packages=['bayeux']
      )
